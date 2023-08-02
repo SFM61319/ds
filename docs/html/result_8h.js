386458805dc39680da59a4bd969e3c42e54f4233
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2023-08-02 10:24:58 +0000

    Generate automated Doxygen documentation

--- docs/html/result_8h.js
+++ docs/html/result_8h.js
@@ -2,6 +2,7 @@ var result_8h =
 [
     [ "DS_RESULT_BYTES", "result_8h.html#afc798b57ff75603187a5a5c11c8eb8bb", null ],
     [ "DS_RESULT_PROPAGATE_ERR", "result_8h.html#ab3468a78eaefe7fbd589612893f125fb", null ],
+    [ "DS_RESULT_PROPAGATE_IF_NULL", "result_8h.html#a319b7f72775ded1a63dc0cfa04ebd128", null ],
     [ "ds_result_t", "result_8h.html#a2dec28ca41a5895eea78b63bd94261a1", [
       [ "DS_RESULT_OK", "result_8h.html#a2dec28ca41a5895eea78b63bd94261a1a62b88ddc31b44e87bef1720a50581560", null ],
       [ "DS_RESULT_ERR_PTR_IS_NULL", "result_8h.html#a2dec28ca41a5895eea78b63bd94261a1a0751741d31129034ab8e720b41b85d53", null ]
